a316aeb7317533b7d841b164443353f8f67eb09e
johnhu <huzichunjohn@126.com>
2014-02-25 11:08:31 +0800

    modify test url.

--- test_selenium.py
+++ test_selenium.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 driver = webdriver.PhantomJS('phantomjs')
 
-driver.get('http://www.qq.com')
+driver.get('http://www.sina.com.cn')
 print driver.title
 
 driver.quit()
