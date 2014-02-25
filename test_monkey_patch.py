9b84b20d98007b61caf7d3f5719f84ccf7be5781
johnhu <huzichunjohn@126.com>
2014-02-25 17:59:17 +0800

    test monkey patch.

---
+++ test_monkey_patch.py
@@ -0,0 +1,15 @@
+#!/bin/env python
+
+class Foo(object):
+    def bar(self):
+        print "Foo.bar"
+            
+def bar(self):
+    print "Modified bar"
+
+Foo().bar()
+
+Foo.bar = bar
+
+Foo().bar()
+
